6683b24d3deb73696f276e44700555ef4e8de978
DH7789 <darnaud2@myges.fr>
2022-07-16 17:00:42 +0200

    fix

---
+++ src/main/java/com/esgi/micro_br_dh/MicroBrDhApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.micro_br_dh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBrDhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBrDhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esgi/micro_br_dh/MicroBrDhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.micro_br_dh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBrDhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
